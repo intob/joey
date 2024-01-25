234ef115c8a3bd2c2801a20d1124f8f0d613a794
Joey Innes <avianpneuma@gmail.com>
2024-01-25 15:00:59 +0100

    Remove crap

--- entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -m # to make job control work
-tor &
-/go/bin/joeyinnes &
-fg %1 # foreground process to prevent killing VM
--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 // We need a http server to serve our Tor requests
 func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
-	port := os.Getenv("PORT")
-	log.Printf("Starting server on http://localhost:%s", port)
-	err := http.ListenAndServe(":"+port, nil)
+	log.Print("Starting server on http://localhost:80\n")
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
