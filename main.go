7ce350713a2e7714e18326422526102cd3084140
Joey Innes <avianpneuma@gmail.com>
2024-01-26 10:18:16 +0100

    Add dir arg to server

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 // We need a http server to serve our Tor requests
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
+	flag.Parse()
+	dir := flag.Arg(0)
+	fs := http.FileServer(http.Dir(dir))
+	log.Printf("serving dir: %s", dir)
 	http.Handle("/", fs)
-	log.Print("Starting server on http://localhost:80\n")
+	log.Print("starting server on http://localhost:80\n")
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
