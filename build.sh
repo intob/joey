025b1444a43b955305302715f00782e043ff8f21
Joey Innes <avianpneuma@gmail.com>
2024-02-24 15:12:21 +0100

    Add service.conf & better build script

--- build.sh
+++ build.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-
 git pull
-
-# get commit hash
+cp service.conf /etc/systemd/system/joeyinnes.service
 export COMMIT_HASH=$(git rev-parse HEAD)
-
-# get hostname
 export HIDDEN_SERVICE_HOSTNAME=$(cat /var/lib/tor/hidden_service/hostname)
-
-# build site
 hugo --baseURL="http://$HIDDEN_SERVICE_HOSTNAME"
-
-# build webserver
-go build
-
-# restart webserver
 systemctl restart joeyinnes
-
-# show status
 systemctl status joeyinnes
